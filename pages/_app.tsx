c5596cdf9d438c934346a5cae3dc7d03afc6bc59
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-02-17 16:40:00 -0700

    Setup Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,42 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#94a3b8", //paragraphs
+    800: "#fff", //titles
+    700: "#172032", //header & bg of course box
+    600: "#0f172a", //dark background color
+  },
+};
+
+const theme = extendTheme({
+  components: {
+    Heading: {
+      baseStyle: {
+        fontWeight: "600",
+      },
+      sizes: {
+        lg: {
+          h: "56px",
+          fontSize: "lg",
+          px: "32px",
+        },
+      },
+    },
+  },
+
+  colors,
+});
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
