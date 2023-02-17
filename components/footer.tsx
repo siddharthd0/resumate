93a598fd1326ea94c0a26379850c0317c6a670d4
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-02-17 16:56:00 -0700

    Navbar and footer finished

---
+++ components/footer.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Box, Container, Stack, Text, Link } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box as="footer" backgroundColor="brand.700" color="brand.900">
+      <Container maxW="6xl" py="12">
+        <Stack direction={{ base: "column", md: "row" }} spacing={4}>
+          <Box flex="1">
+            <Text fontWeight="bold">Resume Builder</Text>
+            <Text fontSize="sm" mt="4">
+              A simple and easy-to-use tool for creating and editing resumes.
+            </Text>
+          </Box>
+          <Box flex="1">
+            <Text fontWeight="bold" mb="4">
+              Links
+            </Text>
+            <Stack spacing={2}>
+              <Link href="/">Home</Link>
+              <Link href="/feedback">Resume Feedback</Link>
+              <Link href="/editor">Resume Editor</Link>
+            </Stack>
+          </Box>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,22 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { Heading} from "@chakra-ui/react"
-import Navigation from '../components/navigation'
-import React from 'react'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Heading } from "@chakra-ui/react";
+import Navigation from "../components/navigation";
+import Footer from "../components/footer";
+import React from "react";
 
 const Home: NextPage = () => {
   const [isOpen, setIsOpen] = React.useState(false);
   const onOpen = () => setIsOpen(true);
   const onClose = () => setIsOpen(false);
   return (
-   <>
-<Navigation onOpen={onOpen} onClose={onClose} isOpen={isOpen} />
-   
-   </>
-  )
-}
+    <>
+      <Navigation onOpen={onOpen} onClose={onClose} isOpen={isOpen} />
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
