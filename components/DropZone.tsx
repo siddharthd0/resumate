90a4a2166f59ad49e4511055d306aa78e3264c94
David Wagih <davidwagih62@gmail.com>
2023-02-18 12:56:51 +0200

    upload feature done

--- components/DropZone.tsx
+++ components/DropZone.tsx
@@ -1,14 +1,23 @@
 import Image from "next/image";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import classes from "./DropZone.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { storage, db } from "../firebase";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc
+} from "firebase/firestore";
+import { ref, getDownloadURL, uploadBytes } from "@firebase/storage";
 
 const DropZone = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
-
-  const [errorMessage, setErrorMessage] = useState("");
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     if (acceptedFiles.length > 1) {
@@ -21,6 +30,27 @@ const DropZone = () => {
       setErrorMessage("");
     }
   }, []);
+
+  const uploadResume = async () => {
+    if (!selectedFile) return;
+
+    const docRef = await addDoc(collection(db, "resumes"), {
+      username: usernameRef.current?.value || "",
+      timestamp: serverTimestamp()
+    });
+    const fileRef = ref(storage, `resumes/${docRef.id}/${selectedFile.name}`);
+    await uploadBytes(fileRef, selectedFile);
+    const downloadURL = await getDownloadURL(fileRef);
+    await updateDoc(doc(db, "resumes", docRef.id), {
+      file: downloadURL
+    });
+
+    if (usernameRef.current) {
+      usernameRef.current.value = "";
+    }
+    setSelectedFile(null);
+    setErrorMessage("");
+  };
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
@@ -40,6 +70,8 @@ const DropZone = () => {
           <p>Drag and drop a PDF file here, or click to select a file</p>
         )}
       </div>
+      <input ref={usernameRef} type="text" placeholder="add your name" />
+      <button onClick={uploadResume}>Upload</button>
     </>
   );
 };
--- pages/uploadResume.tsx
+++ pages/uploadResume.tsx
@@ -3,58 +3,7 @@ import { useDropzone, DropzoneOptions } from "react-dropzone";
 import DropZone from "../components/DropZone";
 
 function UploadResume(): JSX.Element {
-  const [file, setFile] = useState<File | null>(null);
-
-  const handleFileDrop = (acceptedFiles: File[]): void => {
-    if (acceptedFiles.length > 0) {
-      setFile(acceptedFiles[0]);
-    }
-  };
-
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    //accept: "application/pdf",
-    maxFiles: 1,
-    onDrop: handleFileDrop
-  });
-
-  const handleSubmit = async (
-    event: React.FormEvent<HTMLFormElement>
-  ): Promise<void> => {
-    event.preventDefault();
-
-    if (!file) {
-      console.error("No file selected");
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("resume", file, file.name);
-    console.log(formData);
-
-    try {
-      const response = await fetch("/api/uploadResume", {
-        method: "POST",
-        body: formData
-      });
-      console.log(response);
-      // handle the response from the server
-    } catch (error) {
-      console.error("Failed to upload file:", error);
-    }
-  };
-
   return (
-    // <form onSubmit={handleSubmit}>
-    //   <div {...getRootProps()}>
-    //     <input {...getInputProps()} />
-    //     {isDragActive ? (
-    //       <p>Drop the PDF file here ...</p>
-    //     ) : (
-    //       <p>Drag and drop a PDF file here, or click to select a PDF file</p>
-    //     )}
-    //   </div>
-    //   <button type="submit">Submit</button>
-    // </form>
     <div>
       <DropZone />
     </div>
