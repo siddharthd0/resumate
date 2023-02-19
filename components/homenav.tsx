87776609d25fc814d6bcd5be7ff84d66ef3b9047
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-02-19 03:17:39 -0700

    Minor transition fix

--- components/homenav.tsx
+++ components/homenav.tsx
@@ -19,7 +19,7 @@ interface NavigationProps {
 const Navigation: React.FC<NavigationProps> = ({ onOpen, onClose, isOpen }) => {
   const navVariants = {
     hidden: { opacity: 0, y: -50 },
-    visible: { opacity: 1, y: 0, transition: { duration: 1.4, delay: 2 } },
+    visible: { opacity: 1, y: 0, transition: { duration: 1.4, delay: 2.5 } },
   };
 
   const menuItemVariants = {
